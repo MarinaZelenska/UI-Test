6fee928780847560a8ca63a2220ba2ee8b161fc2
MarinaZelenska <51053483+MarinaZelenska@users.noreply.github.com>
2020-10-17 17:16:02 +0300

    Add files via upload

---
+++ GemoTestNG/src/test/java/Lesson22/BaseUITestFile.java
@@ -0,0 +1,27 @@
+package DemoUI;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseUITestFile {
+    public WebDriver driver;
+    public WebDriverWait wait;
+    public String url = "https://rozetka.com.ua/";
+
+    @BeforeClass
+    public void setupDriver(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+
+    }
+
+    @AfterClass
+    public void closeDriver(){
+        driver.quit();
+    }
+}
